fc2bc8315b9f07236585b69d386df57c8b108802
Lou-Anne <saejinmx@gmail.com>
2025-09-18 16:11:23 +0200

    Tri des filtres de la HP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function getHomeDatas()
+    public function getHomeDatas(Request $request)
     {
-        $savedMovies = Movie::all()->load('genres')->where('watched', false);
-        // dd($savedMovies);
-        return view('home', ['savedMovies' => $savedMovies]);
+        $savedMovies = Movie::where('watched', false);
+
+        if ($request->has('genre')) {
+            $savedMovies = $savedMovies->whereHas('genres', function ($query) use ($request) {
+                $query->where('id', $request->input('genre'));
+            });
+            $selectedGenre = Genre::find($request->input('genre'));
+            $selectedGenre = $selectedGenre->name;
+        }
+
+        $savedMovies = $savedMovies->with('genres')->get();
+        $allGenres = Genre::withCount('movies')->get();
+
+        return view('home', ['savedMovies' => $savedMovies, 'allGenres' => $allGenres, 'selectedGenre' => $selectedGenre]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,20 @@
             <span class="alert"><i class='bxr  bx-check-circle'></i>{{ session('error') }}</span>
         @endsession
 
-        <h2>Votre <span class="highlight">Watchlist</span></h2>
+        <div class="watchlistHeader">
+            <h2>Votre <span class="highlight">Watchlist</span></h2> @isset($selectedGenre) 
+            <span>Filtrée par genre : {{ $selectedGenre }}</span> @endisset
+        </div>
+        <form action="{{ route('home') }}" method="get">
+            @csrf
+            <label for="genre">Filtrer par genre :</label>
+            <select name="genre" id="genre">
+                @foreach ($allGenres as $genre)
+                    <option value="{{ $genre->id }}">{{ $genre->name }} ({{ $genre->movies_count }})</option>
+                @endforeach
+            </select>
+            <button type="submit"><i class='bxr  bx-filter'></i></button>
+        </form>
         <div>
             @foreach ($savedMovies as $movie)
                 <div class="homeMovieBox">
